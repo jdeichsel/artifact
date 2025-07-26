3d3882372556a745300486f72cf0960341156665
st183703 <st183703@stud.uni-stuttgart.de>
2025-07-27 00:56:27 +0200

    withdraw & craft
    added new functionalities to player class

--- player.py
+++ player.py
@@ -10,12 +10,12 @@ class Player:
         self.name = name
         self.coords = (None, None)
         self.bank_coords = (4, 1)
+        self.ws_mining_coords = (1, 5)
 
     def get_api_token(self):
         with open("secret.txt") as file:
             token = file.readlines()[0]
         return token
-
 
     def get_post_request(self, action):
         """
@@ -30,7 +30,6 @@ class Player:
         }
         return url, headers
 
-
     def get_get_request(self, action):
         """
         Return appropriate GET request URLs based in action suffix
@@ -44,7 +43,6 @@ class Player:
         }
         return url, headers
 
-
     def get_character_data(self):
         """
         Get all characters' metadata as JSON.
@@ -56,7 +54,6 @@ class Player:
         data = response.json()
         return data
 
-
     def check_banking(self, response):
         """
         If response code 497, player has a full inventory and needs to deposit.
@@ -66,7 +63,6 @@ class Player:
             self.move(*self.bank_coords)
             self.deposit()
             self.move(*self.coords)
-
 
     def check_loss(self, data):
         """
@@ -79,7 +75,6 @@ class Player:
                 self.move(*self.coords)
         except KeyError:
             print(f"[{self.name}]: Could not find data, likely a start-up error")
-
 
     def get_inventory_list(self):
         """
@@ -103,6 +98,25 @@ class Player:
             return inventory
         return None
 
+    def withdraw(self, item, amount):
+        """
+        Withdraw an item with given amount of the bank.
+        Must use the correct item name!
+        :param item:
+        :param amount:
+        """
+        url, headers = self.get_post_request("bank/withdraw/item")
+        json_body = json.dumps([{ "code": item, "quantity": amount }])
+
+        response = requests.post(url, headers=headers, data=json_body)
+        if response.status_code <= 200:
+            data = response.json()
+            print(f"[{self.name}]: Withdrawing {amount}x {item}")
+            self.cooldown_timer(data)
+            return True
+        else:
+            print(f"[{self.name}]: No more {item} in bank!")
+            return False
 
     def deposit(self):
         """
@@ -118,6 +132,23 @@ class Player:
         print(f"[{self.name}]: Dumping inventory")
         self.cooldown_timer(data)
 
+    def craft(self, item, amount):
+        """
+        Craft an item for a specified amount.
+        Must be standing on any crafting place. Must be using correct item codes.
+        """
+        url, headers = self.get_post_request("crafting")
+        json_body = json.dumps({ "code": item, "quantity": amount })
+
+        response = requests.post(url, headers=headers, data=json_body)
+        if response.status_code <= 200:
+            data = response.json()
+            print(f"[{self.name}]: Crafting {amount}x {item}")
+            self.cooldown_timer(data)
+            return True
+        else:
+            print(f"[{self.name}]: Cannot craft! Bad state!")
+            return False
 
     def move(self, x, y):
         """
@@ -132,7 +163,6 @@ class Player:
         data = response.json()
         self.cooldown_timer(data)
 
-
     def fight(self):
         """
         Fight at current place.
@@ -146,7 +176,6 @@ class Player:
         self.check_banking(response)
         self.check_loss(data)
 
-
     def fight_loop(self):
         """
         Keep fighting indefinitely at current place.
@@ -157,7 +186,6 @@ class Player:
             self.fight()
             self.rest()
 
-
     def rest(self):
         """
         Regain all HP
@@ -166,7 +194,6 @@ class Player:
         response = requests.post(url, headers=headers)
         data = response.json()
         self.cooldown_timer(data)
-
 
     def cooldown_timer(self, data):
         """
@@ -181,7 +208,6 @@ class Player:
 
         time.sleep(cooldown)
 
-
     def gather(self):
         """
         Gather resources at current place
@@ -192,7 +218,6 @@ class Player:
         self.cooldown_timer(data)
         self.check_banking(response)
 
-
     def gather_loop(self):
         """
         Gather resources at current place indefinitely
@@ -201,43 +226,25 @@ class Player:
         while True:
             self.gather()
 
-
+    def craft_copper_bars(self):
+        """
+        Craft copper bars in the Mining Workshop (1, 5)
+        Stop when withdrawing copper ore from bank doesnt work
+        """
+        while True:
+            self.move(*self.bank_coords)
+            # if the withdraw fails, theres no more copper and we can stop
+            if not self.withdraw("copper_ore", 80):
+                break
+            self.move(*self.ws_mining_coords)
+            self.craft("copper_bar", 8)
 
 
 
 if __name__ == '__main__':
     pass
-    # MiningMaiden = Player("MiningMaiden")
-    # MiningMaiden.coords = (2, 0)
-    # MiningMaiden.check_banking()
-    # BlueMaiden = Player("BlueMaiden")
-    # BlueMaiden.fight()
+    BlueMaiden = Player("BlueMaiden")
+    BlueMaiden.craft_copper_bars()
     # move("BlueMaiden", 0, 1)
     # fight("BlueMaiden")
     # rest("BlueMaiden")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
