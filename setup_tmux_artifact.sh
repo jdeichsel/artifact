de738ed2486d7751ada444aa47868798d531ced5
st183703 <st183703@stud.uni-stuttgart.de>
2025-07-26 20:27:22 +0200

    Create setup_tmux_artifact.sh
    add bash script to execute each player in seperate tmux windows

---
+++ setup_tmux_artifact.sh
@@ -0,0 +1,14 @@
+SESSION="artifact"
+
+tmux new-session -d -s $SESSION
+
+declare -a PLAYERS=("BlueMaiden" "WoodenMaiden" "MiningMaiden" "FishMaiden" "AlchMaiden")
+ACTIONS=("fight" "gather" "gather" "gather" "gather")
+COORDS=("(0, 1)" "(-1, 0)" "(2, 0)" "(4, 2)" "(2, 2)")
+
+for i in "${!PLAYERS[@]}"; do
+    tmux new-window -t $SESSION: -n "${PLAYERS[i]}"
+    tmux send-keys -t $SESSION:"${PLAYERS[i]}" \
+        "python /home/jan/_py/artifact/run_players.py --player ${PLAYERS[i]} --action ${ACTIONS[i]} --coords '${COORDS[i]}'" C-m
+done
+
